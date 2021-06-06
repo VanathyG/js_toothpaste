146dc4d9ba7b6c6ae92b0c6ef76654e755028fbe
jerielbenavides <33844758+jerielbenavides@users.noreply.github.com>
2021-06-06 15:46:26 -0400

    #2
    Print text to screen

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log(storyTitle);
 //* add an h1 to our html page, then put our title into it.
 let pageBody = document.querySelector("body");
 let storyTitleTag = document.createElement("h1");
-let storycontent = document.createElement("p");
+let storycontentTag = document.createElement("p");
 storyTitleTag.innerText = storyTitle;
 pageBody.appendChild(storyTitleTag);
 
@@ -22,6 +22,8 @@ let leftArm = "left arm";
 let rightArm = "right arm";
 let rightWrist = "right wrist";
 let leftWrist = "leftWrist";
+let leftElbow = "Left Elbow";
+let rightElbow = "Right Elbow";
 
 
 taketoothpaste();
@@ -29,12 +31,48 @@ taketoothpaste();
 //taking toothpast
 function taketoothpaste(){
     moveBodyPart("Stretch", leftArm, 90, "frontways");
+    turnWrist("right", 90, "anticlock");
+    moveBodyPart("Move", rightArm, 30, "down");
+    closeFingersOver(90, items[0]);
+    bendElbow(leftElbow, 45, null);
 }
 
 function moveBodyPart(action, bodyPart, number, direction){
         let step_text = action + " your " + bodyPart + " by " + number + " degrees " + direction
-        console.log(step_text);
+    print_and_log(step_text)
         readytoContinue();
+
+}
+
+function turnWrist(side, degrees, direction){
+    let step_text = "Turn the "+ side + " wrist " + degrees + " " + direction;
+    print_and_log(step_text)
+    readytoContinue();
+}
+
+function  closeFingersOver(degrees, object){
+    let step_text = "Close all your fingers " + degrees + " over " + object;
+    print_and_log(step_text)
+    readytoContinue();
+}
+
+function bendElbow(elbow, degrees, object){
+    let step_text = "Waiting...";
+    if (object == null){
+        step_text = "Bend the " + elbow + " by " + degrees + " degrees.";
+    }
+    else{
+        step_text = "Bend the " + elbow + " by " + degrees + " degrees towards " + object;
+    }
+    print_and_log(step_text)
+    readytoContinue();
+}
+
+function print_and_log (text){
+    console.log(text);
+    let storyContentTag = document.createElement("p");
+    storyContentTag.innerText =" - " + text;
+    pageBody.appendChild(storyContentTag);
 }
 
 function readytoContinue(){
